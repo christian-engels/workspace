d35a7f0d8f20fddf74fd054220803fb14c4ff005
Christian Engels <33321115+christian-engels@users.noreply.github.com>
2025-02-10 23:46:33 +0000

    fix

--- python/hello-world.py
+++ python/hello-world.py
@@ -1,9 +1,14 @@
 import seaborn as sns
+import pyfixest as pf
 import matplotlib.pyplot as plt
 
 # Load an example dataset
 tips = sns.load_dataset("tips")
+tips.head()
 
 # Create a simple plot
 sns.scatterplot(data=tips, x="total_bill", y="tip")
 
+# Run a regression of tip on total bill
+model = pf.feols('tip ~ total_bill', tips)
+model.summary()
